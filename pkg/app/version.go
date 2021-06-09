b553487018ce7e4d7dc11319417d1835c4d2362d
wolfgang mathe <wolfgang.mathe@itdesign.at>
2021-06-09 20:04:55 +0200

    optimizing mqtt

--- pkg/app/version.go
+++ pkg/app/version.go
@@ -18,7 +18,7 @@ import (
 // but we keep the correct syntax.
 //TODO: increase version number to 1.0.1+2020xxyy
 const (
-	VERSION = "1.1.0+20210608"
+	VERSION = "1.1.0+20210608beta"
 	MODULE  = "s0counter"
 )
 
